da0cb5d97fc83b2cdb51644425b25faffe71882a
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2021-07-01 20:21:24 -0300

    Saving the modified file over here. We could copy it with a playbook.

---
+++ autopartes/config.local.mk
@@ -0,0 +1,8 @@
+define SUBPROJECT_REPOS
+https://github.com/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0 \
+https://github.com/reactioncommerce/reaction.git,reaction,v3.11.1 \
+https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.13 \
+https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.3.0 \
+git@github.com:weknowinc/autopartes.git,autopartes,master
+endef
+ 
